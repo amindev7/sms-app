146683bdb8cdd21620a15ae28cece3ff86540300
Amin <hamadagood111@gmail.com>
2020-11-28 18:00:03 +0100

    Add message migration

---
+++ database/migrations/2020_11_28_163706_create_messages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Message;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->text('text');
+            $table->string('status')->default(Message::PENDING);
+            $table->timestamps();
+            $table->unsignedBigInteger('sender_id');
+
+            $table->foreign('sender_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
